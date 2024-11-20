d3ca8bb63a20f1c3b7396bf87f236b8218781fbf
Roger Chavez <Roger.Chavez@chavezroger.com>
2024-11-20 05:28:39 +0000

    Have the base boiler plates for my frontend, backend, database,set up nginx and my certifications; the website is live

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello from PM2-managed server!');
+});
+
+// Example API endpoint
+app.get('/api', (req, res) => {
+  res.send('Hello, this is the API!');
+});
+
+// Other endpoints
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Here is some data!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
